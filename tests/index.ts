aa3d3e118fba3c35e9f3b480fdf1b2fe0c004bb7
Nicolas ROCHON <naroth705@gmail.com>
2023-09-25 21:38:33 +0200

    fix: delete dev file

--- tests/index.ts
+++
@@ -1,9 +0,0 @@
-import { program } from '../src';
-
-program.name("example");
-
-program
-    .command("git")
-    .autocomplete(({ lastWord }) => ["--help", "--version", "clone", "commit", "push", lastWord]);
-
-program.parse();
