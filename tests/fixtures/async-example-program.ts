6a15d8e606b5907caaa56d169c123058ef1517d2
Nicolas ROCHON <naroth705@gmail.com>
2023-11-23 23:32:29 +0100

    fix: add test for parseAsync

---
+++ tests/fixtures/async-example-program.ts
@@ -0,0 +1,26 @@
+import { promises as fs, readdirSync } from 'fs';
+import { program } from '../..';
+
+program.name("example")
+
+const git = program.command("git");
+
+git.command("clone")
+git.command("commit")
+git.command("push");
+
+
+program.command("ls")
+    .autocomplete(async () => {
+        const directoryContent = await fs.readdir(process.cwd(), "utf-8");
+        return directoryContent.sort();
+    })
+
+program.command("lsSync")
+    .autocomplete(() => {
+        const directoryContent = readdirSync(process.cwd(), "utf-8");
+        return directoryContent.sort();
+    })
+
+
+await program.parseAsync(process.argv);
