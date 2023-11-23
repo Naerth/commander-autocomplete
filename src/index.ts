26c9bf6002c44b5fcd6562bebf96ced73c2d686d
Nicolas ROCHON <naroth705@gmail.com>
2023-11-23 23:23:50 +0100

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Command } from './commands/command.js';
 
 export * from './commands/index.js'
 export * from './utils/index.js'
+export * from './types.js'
 
 
 export const program = new Command();
