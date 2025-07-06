b3bfcfbfbf734400f4f93b4453cd83995b6e35b7
Naerth <15349057+Naerth@users.noreply.github.com>
2025-07-06 13:33:23 +0200

    feat: refactor autocomplete function to improve command and option handling with dedicated utility functions

--- src/commands/autocomplete.ts
+++ src/commands/autocomplete.ts
@@ -1,4 +1,8 @@
 import { Command } from "./command.js";
+import { getActiveCommand } from './get-active-command.js';
+import { getActiveOption } from './get-active-option.js';
+import { getAvailableCommandAvailableSuggestions } from './get-available-suggestions.js';
+import { getSuggestions } from './get-suggestions.js';
 
 /**
  * Provides autocomplete for command-line interfaces built with the Commander.js library.
@@ -6,43 +10,64 @@ import { Command } from "./command.js";
  * @returns A promise that resolves to an array of strings to be used for autocomplete.
  */
 export async function autocomplete(...commanderArgs: any[]): Promise<string[] | undefined> {
-
-    const command = (commanderArgs.pop() as Command);
+    const command = commanderArgs.pop() as Command;
     const { parent, args } = command;
-    const allWords = args.splice(1);
-    const lastWord = allWords.slice(-1)[0];
 
-    let activeCommand: Command = parent ?? command;
-    let activeCommandIdx;
-    let excludableFlags: string[] | undefined = undefined;
+    const inputWords = args.slice(1); // skip binary
+    const lastWord = inputWords.at(-1) ?? '';
+    const lastRealWord = inputWords.filter(Boolean).at(-1) ?? '';
 
-    for (let i = 0; i < allWords.length; i++) {
-        const subCommand = activeCommand.getVisibleCommands().find(cmd => cmd.name() === allWords[i]);
-        if (subCommand) {
-            activeCommand = subCommand;
-            activeCommandIdx = i;
-        }
-    }
+    /**
+     * Get the active command based on the input words.
+     */
+    const { activeCommand, activeCommandIdx } = getActiveCommand(inputWords, parent ?? command, 0);
 
-    if (activeCommandIdx !== undefined)
-        excludableFlags = allWords.slice(activeCommandIdx + 1);
+    /**
+     * Get the active option
+     */
+    const activeOption = getActiveOption(inputWords, activeCommand);
 
-    const autoCompleteWords = [
-        ...await activeCommand.complete?.({ lastWord, allWords }) ?? [],
-        ...activeCommand.getVisibleCommands().map(subCommand => subCommand.name()),
-        ...activeCommand.getFlags(excludableFlags),
-    ];
+    /**
+     * Get the remaining words after the active command.
+     * This is used to determine which options and arguments choices have already been used.
+     * The active command is the command that is currently being processed,
+     * and the remaining words are those that come after it in the input.
+     */
+    const remainingWords = inputWords.slice(activeCommandIdx + 1);
 
+    /**
+     * Get excludables words
+     * This includes options and arguments choices that have already been used
+     * after the active command.
+     */
+    const availableSuggestions = getAvailableCommandAvailableSuggestions(remainingWords, activeCommand);
 
-    let filteredAutocompleteWords: string[] = [];
+    /**
+     * Get suggestions based on the active command and option.
+     */
+    const suggestions = await getSuggestions(
+        activeCommand,
+        activeOption,
+        availableSuggestions,
+        inputWords,
+        lastWord,
+        lastRealWord
+    )
+    
+    /**
+     * Filter suggestions based on the last word.
+     * If the last word is empty or matches the active command name, return all suggestions.
+     * Otherwise, filter suggestions that start with the last word and are not equal to the last word.
+     */
+    const filtered = (!lastWord || activeCommand.name() === lastWord)
+        ? suggestions
+        : suggestions.filter(word => word.startsWith(lastWord) && word !== lastWord);
 
-    if (!lastWord || activeCommand.name() === lastWord)
-        filteredAutocompleteWords = autoCompleteWords;
-    else
-        filteredAutocompleteWords = autoCompleteWords.filter(completeWord => completeWord.startsWith(lastWord) && completeWord !== lastWord);
-
-    if (filteredAutocompleteWords.length === 0) return;
-
-    // Sort the autocomplete words so that flags come after commands
-    return filteredAutocompleteWords;
+    /**
+     * Return the filtered suggestions if any exist, otherwise return undefined.
+     * Returning undefined allow the shell to handle the case where no suggestions are available,
+     * Basically, shell will display folders and files in the current directory.
+     * which is useful for the command line interface to handle gracefully.
+     */
+    return filtered.length > 0 ? filtered.toSorted() : undefined;
 }
